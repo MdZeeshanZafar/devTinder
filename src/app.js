81920c4edb185dae246a04b8b64969ebe3ea8cac
Md Zeeshan Zafar <zeeshanzafar2012@gmail.com>
2025-11-17 20:01:15 +0530

    wild-card error handling

--- src/app.js
+++ src/app.js
@@ -7,19 +7,31 @@ const { adminAuth, userAuth } = require('./middlewares/auth')
 
 //Handle Auth Middleware for all type of request (Get, post, patch......)
 
-app.use("/admin", adminAuth)
-app.get('/user', userAuth, (req,res) =>{
-    res.send('User data sent')
+// app.use("/admin", adminAuth)
+// app.get('/user', userAuth, (req,res) =>{
+//     res.send('User data sent')
+// })
+
+// app.get('/admin/getAllData', (req,res) =>{
+//     res.send('All data sent')
+// })
+
+// app.get('/admin/deleteUser', (req,res) =>{
+//     res.send('Deleted a user')
+// })
+
+//error handling
+app.get("/getUserData", (req, res) =>{
+
+    throw new Error("fghjk")
+    res.send("User data sent")
 })
 
-app.get('/admin/getAllData', (req,res) =>{
-    res.send('All data sent')
+app.use("/", (err, req, res, next) => {
+    if(err) {
+        res.status(500).send("Something went wrong ")
+    }
 })
-
-app.get('/admin/deleteUser', (req,res) =>{
-    res.send('Deleted a user')
-})
-
 
 app.listen(PORT || 3000, () => {
     console.log(`Server is up and running on Port ${PORT}.`);
